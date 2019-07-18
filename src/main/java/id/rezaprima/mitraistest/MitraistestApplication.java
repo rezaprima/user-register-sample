6f3834c059164d5775c458a9b18a6d8e499f2e42
Reza Primardiansyah <reza.primardiansyah@gmail.com>
2019-07-18 21:01:08 +0700

    initial commit from spring initializr

---
+++ src/main/java/id/rezaprima/mitraistest/MitraistestApplication.java
@@ -0,0 +1,13 @@
+package id.rezaprima.mitraistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitraistestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitraistestApplication.class, args);
+	}
+
+}
