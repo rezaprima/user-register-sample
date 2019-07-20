7534b14ad69dd1f1789c4bd94b831614bcf97e8c
Reza Primardiansyah <reza.primardiansyah@gmail.com>
2019-07-20 18:57:06 +0700

    create model

---
+++ src/main/java/id/rezaprima/mitraistest/model/User.java
@@ -0,0 +1,79 @@
+package id.rezaprima.mitraistest.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String email;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String mobileNumber;
+	
+	private LocalDate birthdate;
+	
+	private char gender;
+
+	public  String getEmail() {
+		return email;
+	}
+
+	public  void setEmail(String email) {
+		this.email = email;
+	}
+
+	public  String getFirstName() {
+		return firstName;
+	}
+
+	public  void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public  String getLastName() {
+		return lastName;
+	}
+
+	public  void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public  String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public  void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public  LocalDate getBirthdate() {
+		return birthdate;
+	}
+
+	public  void setBirthdate(LocalDate birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public  char getGender() {
+		return gender;
+	}
+
+	public  void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public  Long getId() {
+		return id;
+	}
+	
+	
+}
