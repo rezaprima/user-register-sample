3fe5955c2001b555c21a2eedf8b492870f3d087d
Reza Primardiansyah <reza.primardiansyah@gmail.com>
2019-07-21 14:59:09 +0700

    implement register page

---
+++ src/main/java/id/rezaprima/mitraistest/controller/UserController.java
@@ -0,0 +1,38 @@
+package id.rezaprima.mitraistest.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import id.rezaprima.mitraistest.model.User;
+import id.rezaprima.mitraistest.repo.UserRepository;
+
+@Controller
+public class UserController {
+	private final UserRepository userRepository;
+	
+	@Autowired
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	@GetMapping("/register")
+	public String showRegisterForm(User user) {
+		return "register";
+	}
+	
+	@PostMapping("/register")
+	public String registerUser(@Valid User user, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			return "register";
+		}
+		
+		userRepository.save(user);
+		return "login";
+	}
+}
